452e6d1c29628ce61ae38279ac11515cdd83fc5e
fireburn22 <47121951+fireburn22@users.noreply.github.com>
2019-06-05 20:49:42 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Hello World"
+echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
+echo "AGENT_WORKFOLDER contents:"
+ls -1 $AGENT_WORKFOLDER
+echo "AGENT_BUILDDIRECTORY is $AGENT_BUILDDIRECTORY"
+echo "AGENT_BUILDDIRECTORY contents:"
+ls -1 $AGENT_BUILDDIRECTORY
+echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
+echo "systemdefaultworkingdircotry is:"
+ls -l $(system.defaultworkingdirectory)/target/azure-functions/
+echo "buildartifactsstagingdirectory is:
+ls -l  $(build.artifactstagingdirectory)
+echo "Over and out."
